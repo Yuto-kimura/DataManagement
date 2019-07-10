f38edeb099e5250b8affd205ca444527f11e080e
Yuto-kimura <s1250172a@gmail.com>
2019-07-10 19:32:10 +0900

    Won/Lost Message Added

--- datamanagement.py
+++ datamanagement.py
@@ -6,10 +6,15 @@ class MenuItem:
     def dais(self,daisA,daisB):
         print("Die:"+str(daisA))
         print("Die:"+str(daisB))
+    def hello(self):
+        print("Hello,"+self.name+"!")
 
 
 
 menu_item1 = MenuItem()
+print("What is your name?")
+menu_item1.name = input(">")
+menu_item1.hello()
 menu_item1.road()
 daisA = random.randint(1,6)
 daisB = random.randint(1,6)
@@ -17,3 +22,8 @@ daisB = random.randint(1,6)
 result = daisA + daisB
 menu_item1.dais(daisA,daisB)
 print("Total value:"+str(result))
+
+if result > 7:
+    print("You won!")
+else:
+    print("You lost.")
